71b57ef8351ec1e9c62c4245df1502409e29557d
Andre Bradshaw <bradshaw.andre@gmail.com>
2019-09-19 23:13:39 -0400

    Update clientside.js

--- alternate/clientside.js
+++ alternate/clientside.js
@@ -8,7 +8,7 @@ var delay = (ms) => new Promise(res => setTimeout(res, ms));
 var ele = (t) => document.createElement(t);
 var attr = (o, k, v) => o.setAttribute(k, v);
 
-var appsScriptWebAppLink = 'https://script.google.com/________';
+var appsScriptWebAppLink = '_________________________________________';
 
 function aninCloseBtn() {
   var l1 = tn(this, 'path')[0];
@@ -77,21 +77,22 @@ function convertTime2Secs(str) {
 
 function createYTnotesHTML() {
   if (gi(document, 'youtube_note_search_container')) gi(document, 'youtube_note_search_container').outerHTML = '';
-
+  var docRec = document.body.getBoundingClientRect();
+  var docW = docRec.right - docRec.left;
   var cont = ele('div');
   attr(cont, 'id', 'youtube_note_search_container');
-  attr(cont, 'style', `position: fixed; top: 5%; left: 10%; width: 300px; z-index: 13100;`);
+  attr(cont, 'style', `position: fixed; top: 5%; left: 10%; width: ${Math.ceil(docW*0.4)}px; z-index: 13100;`);
   document.body.appendChild(cont);
 
   var head = ele('div');
-  attr(head, 'style', `display: grid; grid-template-columns: 91% 9%; background: #063842; border: 1.3px solid #021c21; border-top-left-radius: 0.3em; border-top-right-radius: 0.3em; cursor: move; padding: 6px;`);
+  attr(head, 'style', `display: grid; grid-template-columns: 91% 9%; background: #063842; border: 1.3px solid #021c21; border-top-left-radius: 0.3em; border-top-right-radius: 0.3em; cursor: move;`);
   cont.appendChild(head);
   head.onmouseover = dragElement;
 
   var htxt = ele('div');
-  attr(htxt, 'style', `grid-area: 1 / 1; color: #fff;`);
+  attr(htxt, 'style', `grid-area: 1 / 1; color: #fff; padding: 9px;`);
   head.appendChild(htxt);
-  htxt.innerText = 'YouTube Notes';
+  htxt.innerText = 'Source Notes';
 
   var cls = ele('div');
   attr(cls, 'style', `grid-area: 1 / 2; width: 30px; height: 30px; cursor: pointer;`);
@@ -103,7 +104,7 @@ function createYTnotesHTML() {
   cls.onclick = closeView;
 
   var cbod = ele('div');
-  attr(cbod, 'style', `display: grid; grid-template-rows; grid-gap: 10px; border: 1.3px solid #021c21; border-bottom-left-radius: 0.3em; border-bottom-right-radius: 0.3em; background: #9c9c9c; padding: 12px;`);
+  attr(cbod, 'style', `display: grid; grid-template-rows: auto; grid-gap: 10px; border: 1.3px solid #021c21; border-bottom-left-radius: 0.3em; border-bottom-right-radius: 0.3em; background: #9c9c9c; padding: 12px;`);
   cont.appendChild(cbod);
 
   var c3 = ele('div');
@@ -117,7 +118,6 @@ function createYTnotesHTML() {
   var c2 = ele('div');
   attr(c2, 'style', `display: grid; grid-template-columns: 89% 9%; grid-gap: 2%;`);
   cbod.appendChild(c2);
-
 
   var user = ele('input');
   attr(user, 'id', 'save_user_val');
@@ -133,8 +133,11 @@ function createYTnotesHTML() {
   setnote.onkeyup = enterSearch;
 
   var setbtn = ele('div');
-  attr(setbtn, 'style', `grid-area: 1 / 2; border: 1px solid #063842; border-radius: 0.3em; padding: 2px; cursor: pointer; color: #fff; background: #063842; text-align: center; font-size: 1.5em;`);
-  setbtn.innerText = '>';
+  attr(setbtn, 'style', `grid-area: 1 / 2; background: transparent;`);
+  setbtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 294.156 294.156" style="enable-background:new 0 0 294.156 294.156;" xml:space="preserve">
+<g><path d="M227.002,108.256c-2.755-41.751-37.6-74.878-80.036-74.878c-42.447,0-77.298,33.141-80.038,74.907   C28.978,113.059,0,145.39,0,184.184c0,42.234,34.36,76.595,76.595,76.595h116.483c3.313,0,6-2.687,6-6s-2.687-6-6-6H76.595   C40.977,248.778,12,219.801,12,184.184c0-34.275,26.833-62.568,61.087-64.411c3.184-0.171,5.678-2.803,5.678-5.991   c0-0.119-0.003-0.236-0.01-0.355c0.09-37.536,30.654-68.049,68.211-68.049c37.563,0,68.132,30.518,68.211,68.063   c-0.005,0.116-0.009,0.238-0.009,0.329c0,3.196,2.505,5.831,5.696,5.992c34.37,1.741,61.292,30.038,61.292,64.421   c0,19.526-8.698,37.801-23.864,50.138c-2.571,2.091-2.959,5.87-0.868,8.44c2.091,2.571,5.87,2.959,8.44,0.868   c17.98-14.626,28.292-36.293,28.292-59.447C294.156,145.269,265.08,112.926,227.002,108.256z"/><path d="M140.966,141.078v76.511c0,3.313,2.687,6,6,6s6-2.687,6-6v-76.511c0-3.313-2.687-6-6-6S140.966,137.765,140.966,141.078z"/><path d="M181.283,152.204c1.536,0,3.071-0.586,4.243-1.757c2.343-2.343,2.343-6.142,0-8.485l-34.317-34.317   c-2.343-2.343-6.143-2.343-8.485,0l-34.317,34.317c-2.343,2.343-2.343,6.142,0,8.485c2.343,2.343,6.143,2.343,8.485,0   l30.074-30.074l30.074,30.074C178.212,151.618,179.748,152.204,181.283,152.204z"/></g>
+<g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g>
+</g></svg>`;
   c1.appendChild(setbtn);
   setbtn.onclick = createNotesHTML;
   setbtn.onmouseenter = btnhoverin;
@@ -148,8 +151,8 @@ function createYTnotesHTML() {
   searchnote.onkeyup = enterSearch;
 
   var searchbtn = ele('div');
-  attr(searchbtn, 'style', `grid-area: 1 / 2; border: 1px solid #063842; border-radius: 0.3em; padding: 2px; cursor: pointer; color: #fff; background: #063842; text-align: center; font-size: 1.5em;`);
-  searchbtn.innerText = '>';
+  attr(searchbtn, 'style', `grid-area: 1 / 2; background: transparent;`);
+  searchbtn.innerHTML = `<svg enable-background="new 0 0 129 129" viewBox="0 0 129 129" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g><path d="M51.6,96.7c11,0,21-3.9,28.8-10.5l35,35c0.8,0.8,1.8,1.2,2.9,1.2s2.1-0.4,2.9-1.2c1.6-1.6,1.6-4.2,0-5.8l-35-35   c6.5-7.8,10.5-17.9,10.5-28.8c0-24.9-20.2-45.1-45.1-45.1C26.8,6.5,6.5,26.8,6.5,51.6C6.5,76.5,26.8,96.7,51.6,96.7z M51.6,14.7   c20.4,0,36.9,16.6,36.9,36.9C88.5,72,72,88.5,51.6,88.5c-20.4,0-36.9-16.6-36.9-36.9C14.7,31.3,31.3,14.7,51.6,14.7z"/></g></svg>`;
   c2.appendChild(searchbtn);
   searchbtn.onclick = searchNotes;
   searchbtn.onmouseenter = btnhoverin;
@@ -158,12 +161,12 @@ function createYTnotesHTML() {
 }
 
 function btnhoverin() {
-  this.style.background = '#043d47';
+  this.style.transform = 'scale(1.2, 1.2)';
   this.style.transition = 'all 111ms';
 }
 
 function btnhoverout() {
-  this.style.background = '#063842';
+  this.style.transform = 'scale(1, 1)';
   this.style.transition = 'all 111ms';
 }
 
